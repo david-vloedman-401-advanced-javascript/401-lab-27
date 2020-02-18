44c0e166a85e292a1d6b8f1593892da32e9f8f7e
David Vloedman <djvloedman@gmail.com>
2020-02-18 14:52:47 -0800

    homepage update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/david-vloedman-401-advanced-javascript/401-lab-27/precache-manifest.10b1ecea494a08c285ef388e1ab558c6.js"
+  "/precache-manifest.9904be2cbbeb6e5188c8f4011d69fcb9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/david-vloedman-401-advanced-javascript/401-lab-27/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
